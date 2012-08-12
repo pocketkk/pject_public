05fcc3c9d5298d58dc0b13042b4002a44c2c0fa5
pocketkk <pocketkk@gmail.com>
2012-08-11 21:16:32 -0700

    finish sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Pject::Application.routes.draw do
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'static_pages#home'
   
@@ -9,6 +10,8 @@ Pject::Application.routes.draw do
   match '/contact', to: 'static_pages#contact'
   
   match '/signup', to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   
 
   
