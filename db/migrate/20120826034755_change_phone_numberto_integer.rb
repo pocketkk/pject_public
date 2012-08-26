d0694ed584c6e529f98de972960719afc3fa5d0c
pocketkk <pocketkk@gmail.com>
2012-08-26 10:05:22 -0700

    more fixes for migation2

--- db/migrate/20120826034755_change_phone_numberto_integer.rb
+++ db/migrate/20120826034755_change_phone_numberto_integer.rb
@@ -1,7 +1,7 @@
 class ChangePhoneNumbertoInteger < ActiveRecord::Migration
   def change
     rename_column :workorders, :phonenumber, :phonenumber_remove
-    add_columnn :workorders, :phonenumber, :integer
+    add_column :workorders, :phonenumber, :integer
     remove_column :workorders, :phonenumber_remove
   end
 end
