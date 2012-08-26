dfa7bef6ee6a090330e008b21dd847bd2d48f698
pocketkk <pocketkk@gmail.com>
2012-08-26 09:54:29 -0700

    change migration for heroku

--- db/migrate/20120826032248_change_durationto_integer.rb
+++ db/migrate/20120826032248_change_durationto_integer.rb
@@ -1,3 +1,7 @@
 class ChangeDurationtoInteger < ActiveRecord::Migration
-    change_column :workorders, :wo_duration, :integer
+  def change  
+    rename_column :workorders, :wo_duration, :wo_duration_remove
+    add_column :workorders, :wo_duration, :integer
+    remove_column :workorders, :wo_duration_remove
+  end  
 end
