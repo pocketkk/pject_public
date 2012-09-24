079e9b2d89fbce2b6619c2759b56029fe7910f4d
pocketkk <pocketkk@gmail.com>
2012-09-23 20:45:54 -0700

    tweeking lightbox settings

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,12 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require_tree
 //= require lightbox
-//= require_tree .
 
 
 
