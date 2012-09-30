768cda3c49e920be24b3f6f4b6da8acfb2d91830
Jason Crump <pocketkk@gmail.com>
2012-09-30 09:34:21 -0700

    changed show layout-various layout fixes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,8 +11,8 @@ class StaticPagesController < ApplicationController
      @all_workorders=Workorder.wo_current_branch(current_user.current_branch).wo_not_completed.ascending
      @workorders=Workorder.wo_current_branch(current_user.current_branch).wo_not_completed.ascending.paginate(page: params[:page])
      @completed_workorders=Workorder.wo_current_branch(current_user.current_branch).wo_completed.ascending.paginate(page: params[:page]) if signed_in?
-     @updates = Update.all(:order => 'created_at DESC')
-     @assets = Asset.joins(:workorder).where('workorders.branch=350').order('workorders.wo_date ASC')
+     @updates = Update.all(:order => 'created_at DESC', :limit => "15")
+     @assets = Asset.joins(:workorder).where('workorders.branch=350').where('workorders.completed=?',false).order('workorders.wo_date ASC')
      @assets_without_serials = Asset.joins(:workorder).where("workorders.branch=?",current_user.current_branch).needs_serial.order('workorders.wo_date ASC')
   end 
 
