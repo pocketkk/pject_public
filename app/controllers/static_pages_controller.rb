a9e0065129a0d3544f23331f73f6b822d1dcbd9f
Jason Crump <pocketkk@gmail.com>
2012-10-06 19:21:56 -0700

    fixed pagination on default pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
      @workorder = current_user.workorders.build if signed_in?
-     @workorders=Workorder.wo_current_branch(current_user.current_branch).wo_not_completed.ascending.paginate(page: params[:page]) if signed_in?
-     @completed_workorders=Workorder.wo_current_branch(current_user.current_branch).wo_completed.descending.paginate(page: params[:page]) if signed_in?
+     @workorders=Workorder.wo_current_branch(current_user.current_branch).wo_not_completed.ascending.paginate(page: params[:page], :per_page => 5) if signed_in?
+     @completed_workorders=Workorder.wo_current_branch(current_user.current_branch).wo_completed.descending.paginate(page: params[:page], :per_page => 5) if signed_in?
      @old_workorders = current_user.workorders.paginate(page: params[:page], :order => 'wo_date ASC') if signed_in?
      @updates = Update.all(:order => 'created_at DESC', :limit => "15")
    end
