d47ad827690b368a540f495561cefa74b5011a4e
Jason Crump <pocketkk@gmail.com>
2012-10-06 19:25:34 -0700

    changed the recent activity feed from 15 to 25

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,14 +4,14 @@ class StaticPagesController < ApplicationController
      @workorders=Workorder.wo_current_branch(current_user.current_branch).wo_not_completed.ascending.paginate(page: params[:page], :per_page => 5) if signed_in?
      @completed_workorders=Workorder.wo_current_branch(current_user.current_branch).wo_completed.descending.paginate(page: params[:page], :per_page => 5) if signed_in?
      @old_workorders = current_user.workorders.paginate(page: params[:page], :order => 'wo_date ASC') if signed_in?
-     @updates = Update.all(:order => 'created_at DESC', :limit => "15")
+     @updates = Update.all(:order => 'created_at DESC', :limit => "25")
    end
 
    def rebuilder_view
      @all_workorders=Workorder.wo_current_branch(current_user.current_branch).wo_not_completed.ascending
      @workorders=Workorder.wo_current_branch(current_user.current_branch).wo_not_completed.ascending.paginate(page: params[:page])
      @completed_workorders=Workorder.wo_current_branch(current_user.current_branch).wo_completed.descending.paginate(page: params[:page]) if signed_in?
-     @updates = Update.all(:order => 'created_at DESC', :limit => "15")
+     @updates = Update.all(:order => 'created_at DESC', :limit => "25")
      @assets = Asset.joins(:workorder).where('workorders.branch=?', current_user.current_branch).where('workorders.completed=?',false).order('workorders.wo_date ASC')
      @assets_without_serials = Asset.joins(:workorder).where("workorders.branch=?",current_user.current_branch).where('workorders.completed=?',false).serial_blank.order('workorders.wo_date ASC')
   end 
