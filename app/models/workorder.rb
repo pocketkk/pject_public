194b7202aa2157ab346179054c4105aa1253c4ca
Jason Crump <pocketkk@gmail.com>
2012-10-28 21:23:10 -0700

    added branches to BRANCH_OPTIONS

--- app/models/workorder.rb
+++ app/models/workorder.rb
@@ -25,7 +25,7 @@ class Workorder < ActiveRecord::Base
   scope :wo_no_date, where('wo_date IS NULL')
   scope :wo_recently_completed, where('completed = ?', true).limit(4).order("wo_date DESC")
     
-  BRANCH_OPTIONS = ['340','350','360']
+  BRANCH_OPTIONS = ['310','320','330','340','350','360']
   WORKORDER_TYPES = ['New Install','Pull','Swap','Follow Up']
   WORKORDER_TYPES_SEARCH = ['','New Install','Pull','Swap','Follow Up']
   ASSET_STATUS_OPTIONS =  {  "" => "",
