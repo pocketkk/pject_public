066e35a512afe0d7e207af718852d148c6f2f993
Jason Crump <pocketkk@gmail.com>
2012-10-30 19:28:53 -0700

    fixed nil objects in rebuilder view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,7 +11,8 @@ class StaticPagesController < ApplicationController
 
    def rebuilder_view
      @all_workorders=Workorder.wo_current_branch(current_user.current_branch).wo_not_completed.ascending
-     @workorders=Workorder.wo_current_branch(current_user.current_branch).wo_not_completed.ascending.paginate(page: params[:page])
+     @workorders=Workorder.wo_current_branch(current_user.current_branch).wo_not_completed.ascending.paginate(page: params[:page], :per_page => 5) if signed_in?
+     @workorders_without_dates=Workorder.wo_current_branch(current_user.current_branch).wo_no_date.ascending if signed_in?
      @completed_workorders=Workorder.wo_current_branch(current_user.current_branch).wo_completed.descending.paginate(page: params[:page]) if signed_in?
      @updates = Update.all(:order => 'created_at DESC', :limit => "25")
      @assets = Asset.joins(:workorder).where('workorders.branch=?', current_user.current_branch).where('workorders.completed=?',false).order('workorders.wo_date ASC')
