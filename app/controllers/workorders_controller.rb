3b9f963dd07a5f48ed70df0c3599dfaa0d5897c5
Jason Crump <pocketkk@gmail.com>
2012-11-15 21:39:26 -0800

    add follow up workorders to workorder view

--- app/controllers/workorders_controller.rb
+++ app/controllers/workorders_controller.rb
@@ -1,43 +1,43 @@
 class WorkordersController < ApplicationController
   before_filter :signed_in_user
-  
+
   def index
     @search=Workorder.search(params[:q])
-    @workorders = params[:distinct].to_i.zero? ? @search.result.paginate(page: params[:page], :per_page => 6) : @search.result(distinct: true).paginate(page: params[:page], :per_page => 6)  
+    @workorders = params[:distinct].to_i.zero? ? @search.result.paginate(page: params[:page], :per_page => 6) : @search.result(distinct: true).paginate(page: params[:page], :per_page => 6)
   end
-  
+
   def calendar
     @workorders=Workorder.wo_current_branch(current_user.current_branch).where('wo_date IS NOT NULL').ascending
     @workorders_without_dates=Workorder.wo_current_branch(current_user.current_branch).wo_no_date.wo_not_completed.ascending if signed_in?
-    
-    ### workorder lookup for calendar    
+
+    ### workorder lookup for calendar
     @workorders_by_date=@workorders.group_by{|i| i.wo_date.to_date}
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
   end
-    
+
   def create
       @workorder = current_user.workorders.build(params[:workorder])
       if @workorder.save
-        
+
         @update=current_user.updates.new
         @update.feed_item=current_user.name << " created a " << @workorder.wo_type.downcase <<  " workorder for " << @workorder.customer.titleize << "."
         @update.save
-        
+
         @users_to_email_rebuilders=User.where('current_branch=?', current_user.current_branch).where('receive_mails=?', true).where('role=?', 'Rebuilder')
         @users_to_email_branch_manager=User.where('current_branch=?', current_user.current_branch).where('receive_mails=?', true).where('role=?', 'Branch Manager')
         @users_to_email_regional_manager=User.where('current_branch=?', current_user.current_branch).where('receive_mails=?', true).where('role=?', 'Regional Manager')
         @users_to_email=@users_to_email_rebuilders+@users_to_email_branch_manager+@users_to_email_regional_manager
-        
+
         @users_rebuilders=User.where('current_branch=?', current_user.current_branch).where('texts=?', true).where('role=?','Rebuilder')
         @users_branchmanagers=User.where('current_branch=?', current_user.current_branch).where('texts=?', true).where('role=?','Branch Manager')
         @users_regionalmanagers=User.where('current_branch=?', current_user.current_branch).where('texts=?', true).where('role=?','Regional Manager')
         @users_to_text=@users_rebuilders + @users_branchmanagers + @users_regionalmanagers
         flash[:success] = "Workorder created!"
-        
+
         @users_to_email.each do |user|
           PdfMailer.mail_workorder(@workorder,user,"New").deliver
         end
-        
+
         @users_to_text.each do |user|
             client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
               unless user.phone_number.nil?
@@ -49,7 +49,7 @@ class WorkordersController < ApplicationController
                  )
               end
         end
-        
+
         redirect_to root_url
       else
         flash[:error] = 'All fields must be filled to create a new workorder'
@@ -58,46 +58,47 @@ class WorkordersController < ApplicationController
         render :action => 'new'
       end
     end
-  
+
   def destroy
     @workorder=Workorder.find(params[:id])
-   
+
     @update=current_user.updates.new
     @update.feed_item=current_user.name << " deleted the workorder for " << @workorder.customer.titleize << "."
-    
+
     @workorder.destroy
     @update.save
     flash[:success] = "Workorder Deleted"
     redirect_to root_path
   end
-  
+
   def show
     @workorder=Workorder.find(params[:id])
+    @followup_workorder=Workorder.new
     @json=@workorder.to_gmaps4rails
     @assets_need_to_order = Asset.joins(:workorder).where("workorders.branch=?",current_user.current_branch).where('workorders.completed=?',false).where('status=?','0')
-    
+    @users = User.where("current_branch=?",current_user.current_branch)
     @workorder.assigned_to ? @installer=User.find(@workorder.assigned_to) : @no_one_assigned="No one assigned"
-    
+
     @commentable = @workorder
     @comments = @commentable.comments
     @comment = Comment.new
 
   end
-  
+
   def edit
     @workorder=Workorder.find(params[:id])
     @workorder_comparison=Workorder.find(params[:id])
     @users = User.where("current_branch=?",current_user.current_branch)
-    
+
     @workorder.assigned_to ? @installer=User.find(@workorder.assigned_to) : @no_one_assigned="No one assigned"
 
   end
-  
+
   def new
     @workorder=Workorder.new
     @users = User.where("current_branch=?",current_user.current_branch)
   end
-  
+
   def complete
     @workorder = Workorder.find(params[:id])
     if @workorder.update_attributes(:completed => true)
@@ -106,24 +107,24 @@ class WorkordersController < ApplicationController
       @update.feed_item="The workorder for " << @workorder.customer.titleize << " has been completed."
       @update.user_id=current_user.id
       @update.save
-      
+
       @users_branchmanagers=User.where('current_branch=?', current_user.current_branch).where('texts=?', true).where('role=?','Branch Manager')
       @users_regionalmanagers=User.where('current_branch=?', current_user.current_branch).where('texts=?', true).where('role=?','Regional Manager')
-      
+
       @users_to_email_branch_manager=User.where('current_branch=?', current_user.current_branch).where('receive_mails=?', true).where('role=?', 'Branch Manager')
       @users_to_email_regional_manager=User.where('current_branch=?', current_user.current_branch).where('receive_mails=?', true).where('role=?', 'Regional Manager')
       @users_to_email=@users_to_email_branch_manager+@users_to_email_regional_manager
-      
+
       if @workorder.user.receive_mails == true
         PdfMailer.mail_workorder(@workorder,@workorder.user,"Completed").deliver
       end
-      
+
       @users_to_email.each do |user|
         PdfMailer.mail_workorder(@workorder,user,"Completed").deliver
       end
-      
-      
-      if @workorder.user.texts == true 
+
+
+      if @workorder.user.texts == true
         client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
 
              # Create and send an SMS message
@@ -133,9 +134,9 @@ class WorkordersController < ApplicationController
                body: @workorder.customer.titleize << "'s workorder has been completed! " << url_for(@workorder)
              )
       end
-      
+
       @users_to_text=@users_branchmanagers + @users_regionalmanagers
-      
+
       @users_to_text.each do |user|
           unless user==@workorder.user
               client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
@@ -148,21 +149,21 @@ class WorkordersController < ApplicationController
                    )
           end
       end
-      
-    else 
+
+    else
       redirect_to root_path, :error => "Workorder status not updated."
     end
   end
-  
+
   def update_on_calendar
      @workorder= Workorder.find(params[:id])
       @workorder_comparison=Workorder.find(params[:id])
       if @workorder.update_attributes(params[:workorder])
-        @workorder_changes=@workorder.differs_from @workorder_comparison, 
+        @workorder_changes=@workorder.differs_from @workorder_comparison,
                 :ignore_attributes=>['id', 'created_at', 'updated_at', 'latitude','longitude','gmaps']
         @workorder_changes.each do |key, value|
           @update=current_user.updates.new
-          @update.feed_item=current_user.name << " changed " << @workorder.customer.titleize << " from #{value.last} to #{value.first}." 
+          @update.feed_item=current_user.name << " changed " << @workorder.customer.titleize << " from #{value.last} to #{value.first}."
           @update.save
         end
         respond_to do |format|
@@ -174,26 +175,26 @@ class WorkordersController < ApplicationController
       else
         render :action => 'edit'
       end
-    
+
   end
-  
+
   def update
     @workorder= Workorder.find(params[:id])
     @workorder_comparison=Workorder.find(params[:id])
     if @workorder.update_attributes(params[:workorder])
-      @workorder_changes=@workorder.differs_from @workorder_comparison, 
+      @workorder_changes=@workorder.differs_from @workorder_comparison,
               :ignore_attributes=>['id', 'created_at', 'updated_at', 'latitude','longitude','gmaps']
       @workorder_changes.each do |key, value|
         @update=current_user.updates.new
-        @update.feed_item=current_user.name << " changed " << @workorder.customer.titleize << " from #{value.last} to #{value.first}." 
+        @update.feed_item=current_user.name << " changed " << @workorder.customer.titleize << " from #{value.last} to #{value.first}."
         @update.save
       end
       respond_to do |format|
         format.html { redirect_to root_url, :notice  => "Successfully updated workorder." }
         format.js
       end
-     
-       
+
+
     else
       @users = User.where("current_branch=?",current_user.current_branch)
       render :action => 'edit'
--- app/models/workorder.rb
+++ app/models/workorder.rb
@@ -1,9 +1,9 @@
 require 'chronic'
 
 class Workorder < ActiveRecord::Base
-  attr_accessible :customer, :street, :city, :state, :wo_date, 
-                  :wo_duration, :chronic_wo_date, :phonenumber, :raw_phonenumber, 
-                  :contact, :misc_notes, :assets_attributes, :branch, 
+  attr_accessible :customer, :street, :city, :state, :wo_date,
+                  :wo_duration, :chronic_wo_date, :phonenumber, :raw_phonenumber,
+                  :contact, :misc_notes, :assets_attributes, :branch,
                   :before_photos_attributes, :after_photos_attributes, :completed,
                   :wo_type, :assigned_to
   belongs_to :user
@@ -14,9 +14,9 @@ class Workorder < ActiveRecord::Base
   accepts_nested_attributes_for :assets, :allow_destroy => true
   accepts_nested_attributes_for :before_photos, :allow_destroy => true
   accepts_nested_attributes_for :after_photos, :allow_destroy => true
-  
+
   acts_as_gmappable
-  
+
   #scope :current_branch, where("branch=350").order("wo_date ASC")
   scope :wo_current_branch, lambda{ |branch_number| where('branch = ?',
                             branch_number)  }
@@ -27,7 +27,7 @@ class Workorder < ActiveRecord::Base
   scope :wo_has_date, where('wo_date IS NOT NULL')
   scope :wo_no_date, where('wo_date IS NULL')
   scope :wo_recently_completed, where('completed = ?', true).limit(4).order("wo_date DESC")
-    
+
   BRANCH_OPTIONS = ['310','320','330','340','350','360']
   WORKORDER_TYPES = ['New Install','Pull','Swap','Follow Up']
   WORKORDER_TYPES_SEARCH = ['','New Install','Pull','Swap','Follow Up']
@@ -43,7 +43,7 @@ class Workorder < ActiveRecord::Base
                              "Used - Tested"    => "98" ,
                              "Delivered/Not Installed" => "99",
                              "Machine Installed" => "100"}
- 
+
   validates :wo_type, presence: true
   validates :user_id, presence: true
   validates :customer, presence: true
@@ -55,30 +55,30 @@ class Workorder < ActiveRecord::Base
 #  validates :wo_date, presence: true
   validates :wo_duration, presence: true
   validates_length_of :misc_notes, :maximum => 200
-  
+
  def googlemaps_link
    "http://maps.apple.com/maps?q=#{self.gmaps4rails_address.gsub(" ", "%20")}"
  end
- 
+
  def gmaps4rails_address
    "#{self.street}, #{self.city}, #{self.state}"
  end
- 
- 
+
  def chronic_wo_date
    self.wo_date
  end
- 
+
  def chronic_wo_date=(s)
    Chronic.time_class=Time.zone
    self.wo_date = Chronic.parse(s) if s
  end
- 
+
  def raw_phonenumber
    self.phonenumber
  end
- 
+
  def raw_phonenumber=(s)
    self.phonenumber=s.gsub(/\D/, '')
  end
+
 end
