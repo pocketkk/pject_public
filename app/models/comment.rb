ccfe4a50d369c9157fd1ad64e7657f18d029d738
Jason Crump <pocketkk@gmail.com>
2012-12-01 16:41:17 -0800

    changed carrierwave initializer fog credentials to persistence false

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ActiveRecord::Base
   attr_accessible :content
-  
+
   belongs_to :commentable, polymorphic: true
   belongs_to :user
+
+  validates_length_of :content, :maximum => 200
 end
