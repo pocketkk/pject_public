21c2e3de18d6883beb47fe6b2190fefa706db9c3
Jason Crump <pocketkk@gmail.com>
2013-02-23 19:35:44 -0800

    layout changes to mobile

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
-  
+
   # Returns the Gravatar (http://gravatar.com/) for the given user.
   def gravatar_for(user, options = { size: 100 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
@@ -7,15 +7,15 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar dropshadow")
   end
-  
+
   def name_of_user(id)
     user=User.find(id)
     "#{user.name}"
   end
-  
+
   def first_name(user)
     first_name=user.name.split(/\W+/).first
     "#{first_name.titleize}"
   end
-  
+
 end
