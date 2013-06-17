24a52aba7fa2f9a5390566df8c7a86647cd67102
Jason Crump <pocketkk@gmail.com>
2013-06-16 18:07:41 -0700

    added menu icons for mobile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,56 @@ module ApplicationHelper
     end
   end
 
+  def menu_item(menu_item, delete=false)
+    @menu_item = menu_item
+    define_path
+    content_tag :div, :class => "mobile_icons_wrapper" do
+        content_tag(:div, icon(delete), :class => "mobile_icons_top") +
+        content_tag(:div, icon_text(delete), :class => "mobile_icons_bottom")
+    end
+  end
 
+  def icon(delete)
+    if delete
+        link_to(image_tag("/icons/#{@menu_item}_128.png", :size => "40x40"), @icon_path, method: "delete")
+    else
+        link_to(image_tag("/icons/#{@menu_item}_128.png", :size => "40x40"), @icon_path)
+    end
+  end
+
+  def icon_text(delete)
+    if delete
+        link_to @menu_item.titleize, @icon_path, method: "delete"
+    else
+        link_to @menu_item.titleize, @icon_path
+    end
+  end
+
+  def define_path
+    case @menu_item
+        when "Home"
+            @icon_path=root_path
+        when "Calendar"
+            @icon_path=calendar_path
+        when "Search"
+            @icon_path=search_path
+        when "Knowledge"
+            @icon_path=documents_path
+        when "Settings"
+            @icon_path=edit_user_path(current_user)
+        when "Videos"
+            @icon_path=videos_path
+        when "Blog"
+            @icon_path=posts_path
+        when "Bug"
+            @icon_path=new_bug_path
+        when "Desktop"
+            @icon_path=toggle_normal_path
+        when "Logout"
+            @icon_path=signout_path
+        when "New_workorder"
+            @icon_path=new_workorder_path
+        end
+  end
 
 end
