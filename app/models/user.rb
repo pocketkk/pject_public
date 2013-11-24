09c46ffe9d7387496e48e055846d9d39db747cc6
Jason Crump <pocketkk@gmail.com>
2013-11-23 17:54:44 -0800

    removed default scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,7 +42,6 @@ class User < ActiveRecord::Base
     :if => :should_validate_password?
 
 
-  default_scope { where(active: true) }
   scope :active_by_branch, lambda{ |branch| where(:active => true).
     where('current_branch = ?', branch) }
   scope :inactive_by_branch, lambda{ |branch| where(:active => false).
