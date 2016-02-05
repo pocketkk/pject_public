64225aca36a8af0a245c27cbab9cb6db1bd1fe55
pocketkk <pocketkk@gmail.com>
2016-02-05 15:54:46 -0800

    remove config folder

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Pject::Application.config.secret_token = '5fc39c0d047fa62b4d89279592612f108970c10dcf7bdfaecb206e1073035a8888a5c1af6505e125fdad5322ef458a1a9898ecede2c3bb4914ac6bae9aa91590'
